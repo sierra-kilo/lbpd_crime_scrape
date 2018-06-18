f464c43944e38ad1925d60549e66691f0f9c9ccf
Sierra Kilo <sierrakilo@Sierras-MBP.webpass.net>
2018-06-17 19:45:40 -0700

    now creates and exports data to a new file

--- src/lbpd_crime_scrape.py
+++ src/lbpd_crime_scrape.py
@@ -3,7 +3,18 @@
 # with a user input date range
 
 from urllib.request import urlopen as uReq
-import json
+import json, time
+
+def create_export_file():
+    ts = str(time.time()).replace('.', '_')
+    filename = ts + '.csv'
+    f = open(filename, 'w')
+    headers = 'CRIME, TIME, LOCATION, CITY\n'
+    f.write(headers)
+    return f
+
+def export_data(f, crime, time, location, city):
+    f.write(crime + ',' + time + ',' + location + ',' + city + '\n')
 
 def intro():
     print()
@@ -46,6 +57,18 @@ def get_url():
     my_url = 'https://services6.arcgis.com/yCArG7wGXGyWLqav/arcgis/rest/services/Police_Crime_Mapping/FeatureServer/0/query?f=json&where=((REPORTED_DATE_TIME%20BETWEEN%20%27' + start + '%2008%3A00%3A00%27%20AND%20%27' + end + '%2007%3A59%3A59%27))&returnGeometry=true&spatialRel=esriSpatialRelIntersects&geometry=%7B%22xmin%22%3A-20037508.342788905%2C%22ymin%22%3A-1199589.7175994702%2C%22xmax%22%3A20037508.342788905%2C%22ymax%22%3A12263111.200208541%2C%22spatialReference%22%3A%7B%22wkid%22%3A102100%7D%7D&geometryType=esriGeometryEnvelope&inSR=102100&outFields=UCR_LABEL%2CREPORTED_DATE_TIME%2CDR%2CLOCATION%2CCITY%2CDIVISION%2CBEAT%2CRD%2COBJECTID&orderByFields=OBJECTID%20ASC&outSR=102100'
     return my_url
 
+# get data and write to file
+def guts(f, crime_data_all):
+        # loop through and print crime type
+        for index in range(len(crime_data_all)):
+            crime = crime_data_all[index]['attributes']['UCR_LABEL']
+            time =str( crime_data_all[index]['attributes']['REPORTED_DATE_TIME'])
+            location = crime_data_all[index]['attributes']['LOCATION']
+            city = crime_data_all[index]['attributes']['CITY']
+            # print(crime, time, location, city)
+            # export data duh
+            export_data(f, crime, time, location, city)
+
 def main():
     intro()
     my_url = get_url()
@@ -58,12 +81,11 @@ def main():
     # extract crime data
     parsed_total_data = json.loads(raw_total_data)
     crime_data_all = parsed_total_data['features']
-    # loop through and print crime type
-    for index in range(len(crime_data_all)):
-        crime = crime_data_all[index]['attributes']['UCR_LABEL']
-        time = crime_data_all[index]['attributes']['REPORTED_DATE_TIME']
-        location = crime_data_all[index]['attributes']['LOCATION']
-        city = crime_data_all[index]['attributes']['CITY']
-        print(crime, time, location, city)
+    # create csv file
+    f = create_export_file()
+    # get data and export
+    guts(f, crime_data_all)
+    # close file
+    f.close()
 
 main()
